988db6066839e9c028b227e666600061b844e2b1
Matt Davis <mdavis.tles@gmail.com>
2019-08-03 06:13:31 -0700

    Revert "Change how app establishes configurations"
    This reverts commit 51269124880cae1f6f95a1a86704106425cad495.

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-import os
 from flask import Flask
 
-from config import config
 from extensions import db, migrate, sse, login_manager, bcrypt
 
 
-def create_app(config_name):
+def create_app(config_object="settings"):
     """Create application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
     :param config_object: The configuration object to use.
     """
     app = Flask(__name__.split(".")[0])
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    app.config.from_object(config_object)
     register_extensions(app)
     app.register_blueprint(sse, url_prefix="/stream")
     return app
@@ -25,7 +22,7 @@ def register_extensions(app):
     bcrypt.init_app(app)
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app()
 
 # Needs to import after app creation to avoid circular import errors
 from views import *
