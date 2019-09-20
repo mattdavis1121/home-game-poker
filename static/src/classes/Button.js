a22416fc2811b4566e1609437d13dcfb604d8be7
Matt Davis <mdavis.tles@gmail.com>
2019-09-20 08:56:46 -0700

    Defer all button updates until re-enabled
    Also went through and added docs to all the functions.

--- static/src/classes/Button.js
+++ static/src/classes/Button.js
@@ -13,39 +13,88 @@ class Button extends Phaser.Button {
     constructor(game, x, y, key, callback, callbackcontext, overFrame, outFrame, downFrame, upFrame) {
         super(game, x, y, key, callback, callbackcontext, overFrame, outFrame, downFrame, upFrame);
 
-        this.padding = 0;
-        this.label = new Phaser.Text(this.game, 0, 0, "");
+        this.enabled = true;
+        this.labelPadding = 0;
+        this.labelText = "";
+        this.labelStyle = {};
+        this.label = new Phaser.Text(this.game, 0, 0, this.labelText);
         this.addChild(this.label);
 
         // Must add to game world manually if not using game.add.button
         this.game.world.add(this);
     }
 
-    setText(text) {
-        this.label.text = text;
-        this.rePosLabel();
+    /**
+     * @summary Set the text displayed on the button
+     * @param {string} text - The text to display
+     * @param {boolean} force - Force display update despite of this.enabled?
+     */
+    setText(text, force = false) {
+        this.labelText = text;
+        this.updateLabel(force);
     }
 
-    setTextStyle(style) {
-        this.label.setStyle(style);
-        this.rePosLabel();
+    /**
+     * @summary Set the style for the button text
+     * @param {object} style - The text style to use
+     * @param {boolean} force - Force display update despite of this.enabled?
+     */
+    setTextStyle(style, force = false) {
+        this.labelStyle = style;
+        this.updateLabel(force);
     }
 
-    setPadding(padding) {
-        this.padding = padding;
-        this.rePosLabel();
+    /**
+     * @summary Set the padding between the text and the button perimeter
+     * @param {number} padding - The padding in pixels
+     * @param {boolean} force - Force display update despite of this.enabled?
+     */
+    setPadding(padding, force = false) {
+        this.labelPadding = padding;
+        this.updateLabel(force);
     }
 
+    /**
+     * @summary Enable or disable the button
+     * On disable, disables all input to the button and renders it grayed
+     * out. All updates are delayed until re-enable, unless forced.
+     * @param {boolean} enabled - Enable or disable button?
+     */
     setEnabled(enabled) {
+        this.enabled = enabled;
         this.inputEnabled = enabled;
         let tint = enabled ? 0xFFFFFF : 0x808080;
         this.tint = tint;
         this.label.tint = tint;
+
+        // Update on re-enable
+        if (enabled) {
+            this.updateLabel();
+        }
     }
 
+    /**
+     * @summary Update all button attributes to current properties
+     *
+     * If the button is disabled, this will have no effect. The
+     * developer may optionally choose to force the update.
+     *
+     * @param {boolean} force - Force the update?
+     */
+    updateLabel(force = false) {
+        if (this.enabled || force) {
+            this.label.text = this.labelText;
+            this.label.setStyle(this.labelStyle);
+            this.rePosLabel();
+        }
+    }
+
+    /**
+     * @summary Scale label text to fit on button and center
+     */
     rePosLabel() {
         this.label.scale.setTo(1);
-        let textArea = this.width - this.padding * 2;
+        let textArea = this.width - this.labelPadding * 2;
         if (this.label.width > textArea) {
             let reducedScale = textArea / this.label.width;
             this.label.scale.setTo(reducedScale);
