ff8548bfe9b54e1224ad3cd076f365b781b29f9e
Matt Davis <mdavis.tles@gmail.com>
2019-11-27 06:23:42 -0800

    Implement nameplate flashing

--- static/src/classes/Nameplate.js
+++ static/src/classes/Nameplate.js
@@ -11,9 +11,10 @@ class Nameplate extends Phaser.Image {
         this.config = config || this.game.config.nameplate;
 
         this.display = {
-            "nameplate": null,
-            "name": null,
-            "balance": null
+            nameplate: null,
+            name: null,
+            balance: null,
+            flash: null
         };
     }
 
@@ -27,13 +28,30 @@ class Nameplate extends Phaser.Image {
         this.display.balance = new Label(this.game, this.config.balance.x, this.config.balance.y, "", this.config.balance.style);
         this.display.balance.setTextBounds(0, 0, this.display.nameplate.width - 20, 0);
         this.addChild(this.display.balance);
+
+        this.display.flash = new Label(this.game, this.display.nameplate.centerX, this.display.nameplate.centerY, "", this.config.flash.style);
+        this.display.flash.setTextBounds(0, 0, this.display.nameplate.width - 20, 0);
+        this.display.flash.anchor.setTo(0.5);
+        this.display.flash.visible = false;
+        this.addChild(this.display.flash);
     }
 
     /**
-     * @summary Flash text
+     * @summary Flash text for duration
+     * @param {string} text - The text to displays
+     * @param {number} [duration=2000] - Milliseconds to display text
      */
-    flash() {
+    flash(text, duration = 2000) {
+        this.display.name.visible = false;
+        this.display.balance.visible = false;
+        this.display.flash.visible = true;
+        this.display.flash.setText(text);
 
+        this.game.time.events.add(duration, () => {
+            this.display.name.visible = true;
+            this.display.balance.visible = true;
+            this.display.flash.visible = false;
+        }, this);
     }
 
     set name(name) {
